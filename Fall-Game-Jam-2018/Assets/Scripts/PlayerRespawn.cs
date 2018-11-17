421e0177ef144a8b9d1ad9115f99abc4e419a4b5
Stefan Brynielsson <stefan.brynielsson@gmail.com>
2018-11-17 12:00:15 +0100

    Player respawn system implemented

---
+++ Fall-Game-Jam-2018/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour {
+
+    public Transform spawnPoint;
+
+    private Rigidbody rb;
+    private bool playerDied;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (rb.position.y <= -5)
+        {
+            playerDied = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerDied)
+        {
+            rb.MovePosition(spawnPoint.position);
+            rb.velocity=new Vector3(0, 0, 0);
+            playerDied = false;
+        }
+
+    }
+}
