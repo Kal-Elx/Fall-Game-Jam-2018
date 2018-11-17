a8047d7bc0246bc1db4aea4640542e5a14dfca07
Stefan Brynielsson <stebr364@student.liu.se>
2018-11-17 10:30:34 +0100

    Lagt till player object och satt upp movement

---
+++ Fall-Game-Jam-2018/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    
+    public float speed;
+
+    private Rigidbody rb;
+    private Vector3 playerOldPosition, playerOldVelocity;
+    public bool isPlayerOldPositionSet = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerOldPosition = rb.position;
+        playerOldVelocity = rb.velocity;
+    }
+
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!isPlayerOldPositionSet)
+            {
+                playerOldPosition = rb.position;
+                playerOldVelocity = rb.velocity;
+
+                isPlayerOldPositionSet = true;
+
+            }
+            else
+            {
+                rb.MovePosition(playerOldPosition);
+                rb.velocity = playerOldVelocity;
+                isPlayerOldPositionSet = false;
+            }
+        }
+
+
+    }
+}
